a95bf273e3f893e3720beb66292d8306c0f05f51
just4test <54liuz@gmail.com>
2015-10-07 08:47:54 +0800

    为add-digits.rb 增加一种快速解法

--- add-digits.rb
+++ add-digits.rb
@@ -1,6 +1,10 @@
 # @param {Integer} num
 # @return {Integer}
 def add_digits(num)
+    # O(1)的解法
+	return 0 if 0 == num
+	return (num % 9) == 0 ? 9 : num % 9
+	# 通常解法
     ret = 0
 	loop do
 		while 0 != num do
