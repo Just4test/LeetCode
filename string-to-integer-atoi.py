8f1041cf3943be05fb34ed3414947f7c8b0291c0
just4test <54liuz@gmail.com>
2015-10-21 16:52:52 +0800

    new file string-to-integer-atoi.py

---
+++ string-to-integer-atoi.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        INT_MAX = 2147483647
+        INT_MIN = -2147483648
+        
+        begin = 0
+        end = 0
+        flag = False
+        for i, ch in enumerate(str):
+            print ch, cmp(ch, '0'), cmp(ch, '9')
+            if not flag and ch == ' ':
+                continue
+            if ch == '-' or ch == '+':
+                if not flag:
+                    flag = True
+                    begin = i
+                    continue
+                else:
+                    end = i
+                    break
+            if cmp(ch, '0') >= 0 and cmp(ch, '9') <= 0:
+                flag = True
+            else:
+                end = i
+                break
+        if end == 0:
+            end = len(str)
+        try:
+            result = int(str[begin:end])
+            if result > INT_MAX:
+                return INT_MAX
+            if result < INT_MIN:
+                return INT_MIN
+            return result
+        except:Exception
+        return 0
